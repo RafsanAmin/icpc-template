a2b71723556e8392d11ccd60d88e1974be84d7e7
HRM Rafsan Amin <59786405+RafsanAmin@users.noreply.github.com>
2026-05-15 12:59:33 +0600

    Update template.cpp

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define debug(...)
 #endif
 #define FASTIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define sz(v) (int)(begin(v), end(v))
 #define all(v) begin(v), end(v)
 #define pb push_back
@@ -12,6 +13,9 @@ using namespace std;
 using ll = long long; using ld = long double; using pii = pair<int,int>; using pll = pair<ll, ll>;
 const int INF = 2e9 + 42069; const ll INFLL = (ll)INF * (ll)INF; const ld EPS = 10e-9;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+
 #define rand(l, r) uniform_int_distribution<int>(l, r)(rng)
 #define randll(l, r) uniform_int_distribution<ll>(l, r)(rng)
 #define shuffle(v) shuffle(all(v), rng)
